231687425503d4b0ecf47a944fe47765fd8eaa98
Bruno Correia <brunocs@msn.com>
2018-06-04 05:16:29 -0300

    Add MongoDB connection and user model

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const app = require('express')();
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('API Server Running');
-});
+const app = require('express')();
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+const dbConfig = require('./config/database');
+
+mongoose.connect(dbConfig.url);
+requireDir(dbConfig.modelsPath);
+
+app.use('/', require('./app/routes'));
 
 app.listen(3000);
