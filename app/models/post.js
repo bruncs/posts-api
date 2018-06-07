0a3739ae309ad7e64471ca75619efc18179b3579
Bruno Correia <brunocs@msn.com>
2018-06-06 23:58:55 -0300

    Add post model

---
+++ app/models/post.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+mongoose.model('Post', PostSchema);
