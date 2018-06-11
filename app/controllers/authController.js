76660783e838cdafca597bd56e49ef15a40026ea
Bruno Correia <brunocs@msn.com>
2018-06-11 01:42:06 -0300

    Add user token auth and signin method

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -3,6 +3,29 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
 module.exports = {
+  async signin(req, res, next) {
+    try {
+      const { email, password } = req.body;
+
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ error: 'Email not found.' });
+      }
+
+      if (!(await user.compareHash(password))) {
+        return res.status(400).json({ error: 'Invalid password.' });
+      }
+
+      return res.json({
+        user,
+        token: user.generateToken(),
+      });
+    } catch (err) {
+      return next(err);
+    }
+  },
+
   async signup(req, res, next) {
     try {
       const { email } = req.body;
@@ -12,7 +35,10 @@ module.exports = {
       }
       const user = await User.create(req.body);
 
-      return res.json(user);
+      return res.json({
+        user,
+        token: user.generateToken(),
+      });
     } catch (err) {
       return next(err);
     }
---
+++ config/auth.js
@@ -0,0 +1,3 @@
+module.exports = {
+  secret: 'bf5a67cbeaa72afa33f6756d3178163c',
+};
