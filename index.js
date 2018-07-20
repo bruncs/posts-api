8743f0ea1613332639d06b2c61cde2d9a0361396
Bruno Correia <brunocs@msn.com>
2018-07-19 23:33:58 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
 const app = require('express')();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const bodyParser = require('body-parser');
 
 const dbConfig = require('./config/database');
+
+app.use(cors());
 
 mongoose.connect(dbConfig.url);
 requireDir(dbConfig.modelsPath);
